86996f3af05aefbffebd19ccc9d78a2f8ac95710
Shein0425 <luisqui300@gmail.com>
2023-04-21 08:36:34 -0500

    NavBar Final

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -4,104 +4,100 @@ import { OnDestroy, HostListener } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit, OnDestroy {
-    private toggleButton: any;
-    private sidebarVisible: boolean;
+export class NavbarComponent implements OnInit {
+  private toggleButton: any;
+  private sidebarVisible: boolean;
 
-    elem: any; isFullScreen: boolean;
-    constructor(public location: Location, private element : ElementRef, @Inject(DOCUMENT) private document: any) {
-        this.sidebarVisible = false;
-    }
-    ngOnDestroy(): void {
-        throw new Error('Method not implemented.');
-    }
-    ngOnInit() {
-        this.chkScreenMode();
-        this.elem = document.documentElement;
-    }
-    sidebarOpen() {
-        const toggleButton = this.toggleButton;
-        const html = document.getElementsByTagName('html')[0];
-        setTimeout(function(){
-            toggleButton.classList.add('toggled');
-        }, 500);
-        html.classList.add('nav-open');
+  elem: any; isFullScreen: boolean;
+  constructor(public location: Location, private element: ElementRef, @Inject(DOCUMENT) private document: any) {
+    this.sidebarVisible = false;
+  }
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+    this.chkScreenMode();
+    this.elem = document.documentElement;
+  }
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName('html')[0];
+    setTimeout(function () {
+      toggleButton.classList.add('toggled');
+    }, 500);
+    html.classList.add('nav-open');
 
-        this.sidebarVisible = true;
-    };
-    sidebarClose() {
-        const html = document.getElementsByTagName('html')[0];
-        // console.log(html);
-        this.toggleButton.classList.remove('toggled');
-        this.sidebarVisible = false;
-        html.classList.remove('nav-open');
-    };
-    sidebarToggle() {
-        // const toggleButton = this.toggleButton;
-        // const body = document.getElementsByTagName('body')[0];
-        if (this.sidebarVisible === false) {
-            this.sidebarOpen();
-        } else {
-            this.sidebarClose();
-        }
-    };
-  
-    isDocumentation() {
-        var titlee = this.location.prepareExternalUrl(this.location.path());
-        if( titlee === '/documentation' ) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    this.sidebarVisible = true;
+  };
+  sidebarClose() {
+    const html = document.getElementsByTagName('html')[0];
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    html.classList.remove('nav-open');
+  };
+  sidebarToggle() {
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
     }
-    @HostListener('document:fullscreenchange', ['$event'])
-   @HostListener('document:webkitfullscreenchange', ['$event'])
-   @HostListener('document:mozfullscreenchange', ['$event'])
-   @HostListener('document:MSFullscreenChange', ['$event'])
-fullscreenmodes(event){
-      this.chkScreenMode();
+  };
+
+  isDocumentation() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee === '/documentation') {
+      return true;
     }
-chkScreenMode(){
-      if(document.fullscreenElement){
-        //fullscreen
-        this.isFullScreen = true;
-      }else{
-        //not in full screen
-        this.isFullScreen = false;
-      }
+    else {
+      return false;
     }
-openFullscreen() {
-        if (this.elem.requestFullscreen) {
-          this.elem.requestFullscreen();
-        } else if (this.elem.mozRequestFullScreen) {
-          /* Firefox */
-          this.elem.mozRequestFullScreen();
-        } else if (this.elem.webkitRequestFullscreen) {
-          /* Chrome, Safari and Opera */
-          this.elem.webkitRequestFullscreen();
-        } else if (this.elem.msRequestFullscreen) {
-          /* IE/Edge */
-          this.elem.msRequestFullscreen();
-        }
-      }
-/* Close fullscreen */
-      closeFullscreen() {
-        if (this.document.exitFullscreen) {
-          this.document.exitFullscreen();
-        } else if (this.document.mozCancelFullScreen) {
-          /* Firefox */
-          this.document.mozCancelFullScreen();
-        } else if (this.document.webkitExitFullscreen) {
-          /* Chrome, Safari and Opera */
-          this.document.webkitExitFullscreen();
-        } else if (this.document.msExitFullscreen) {
-          /* IE/Edge */
-          this.document.msExitFullscreen();
-        }
-      }
+  }
+  @HostListener('document:fullscreenchange', ['$event'])
+  @HostListener('document:webkitfullscreenchange', ['$event'])
+  @HostListener('document:mozfullscreenchange', ['$event'])
+  @HostListener('document:MSFullscreenChange', ['$event'])
+  fullscreenmodes(event) {
+    this.chkScreenMode();
+  }
+  chkScreenMode() {
+    if (document.fullscreenElement) {
+      //fullscreen
+      this.isFullScreen = true;
+    } else {
+      //not in full screen
+      this.isFullScreen = false;
+    }
+  }
+  openFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
+  }
+  /* Close fullscreen */
+  closeFullscreen() {
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
+  }
 }
